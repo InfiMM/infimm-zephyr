0959690a9b74cd1f16e434b6bf1a4a0bdcbe4b6a
Xiaotian Han <xiaotianhan@users.noreply.huggingface.co>
2024-01-04 22:38:42 +0000

    Delete convert_infi_zephyr_weights_to_hf.py

--- convert_infi_zephyr_weights_to_hf.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-state_dict = torch.load(
-    "cruise_logs/zephyr_freeze_ift/mp_rank_00_model_states.pt", map_location="cpu"
-)
-state_dict = {k.replace("module.", ""): v for k, v in state_dict["module"].items()}
